ebb580afc2ae0987736a2f45568e7e0d16a27b17
LuisPaulo1 <csluispaulo@gmail.com>
2024-11-30 22:42:05 -0300

    Criando componentes do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css';
+
+import { useState } from 'react';
+
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import { wordsList } from './data/words';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
 
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words, setWords] = useState(wordsList);
+
   return (
     <div className="App">
-      <StartScreen />
+      { gameStage === 'start' && <StartScreen /> }
+      { gameStage === 'game' && <Game /> }
+      { gameStage === 'end' && <GameOver /> }
     </div>
   );
 }
